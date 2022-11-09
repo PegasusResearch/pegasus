84e6df062f7d80240b0dd76dfd07d8e60273653c
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2022-11-08 19:21:52 -0500

    feat: documentation generation using sphinx

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
+
+project = 'Pegasus'
+copyright = '2022, Marcelo Jacinto'
+author = 'Marcelo Jacinto'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Add support for ["markdown .md files parsing", "read the docs"]
+extensions = ["myst_parser", "sphinx_rtd_theme", "sphinx.ext.autodoc"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
