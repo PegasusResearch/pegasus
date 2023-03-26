28f8ccf0b55daeaaaf6c68d26c0a768edb3d3436
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2023-03-26 20:43:02 +0100

    fix: misc

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# pegasus
+# Pegasus
 Open-source system for the development of new drone controllers
-
-Authors:
-- Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
-
-Based on previous contributions from:
-- Pedro Trindade <pedro.trindade@isr.tecnico.ulisboa.pt>
--- install_requirements.sh
+++ install_requirements.sh
@@ -37,33 +37,3 @@ sudo make install && \
 cd .. && \
 cd .. && \
 sudo rm -R eigen-3.4.0 eigen-3.4.0.tar.gz
-
-# -------------------------------------------------
-# Install the documentation generation requrirement
-# -------------------------------------------------
-sudo apt-get update && \
-     apt-get upgrade -y && \
-     apt-get install -y --no-install-recommends \
-     doxygen \
-     && rm -rf /var/lib/apt/lists/*
-
-pip3 install --no-cache-dir \
-    mkdocs \
-    mkdocs-material \
-    mkdocs-bibtex \
-    mkdocs-git-revision-date-plugin \
-    mkdocs-git-revision-date-localized-plugin \
-    mkdocs-monorepo-plugin \
-    mkdocs-macros-plugin \
-    mkdocs-include-markdown-plugin \
-    mkdocs-git-revision-date-localized-plugin \
-    mkdocs-include-dir-to-nav \
-    mike \
-    ruamel.yaml
-
-mkdir doxybook2 && \
-    cd doxybook2 && \
-    wget https://github.com/matusnovak/doxybook2/releases/download/v1.4.0/doxybook2-linux-amd64-v1.4.0.zip && \
-    unzip doxybook2-linux-amd64-v1.4.0.zip && \
-    cd bin && mv doxybook2 /usr/bin/ && \
-    cd ${HOME} && rm -f -r doxybook2
