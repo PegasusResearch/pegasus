6fb5a95b295420655957bbfd8d4a4931e43f1f5e
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2023-04-01 22:38:36 +0100

    fix: cleaning up the camera api

--- pegasus_vision/camera_models/include/camera_models/camera.hpp
+++ pegasus_vision/camera_models/include/camera_models/camera.hpp
@@ -23,59 +23,48 @@ public:
     /**
      * @brief Method that takes a 3D point expressed in the Camera coordinate frame,
      * projects it to the image plane (using the intrinsics), and outputs the pixels
-     * corresponding to that point in the image plane
-     * @param x The X coordinate of the point in the Camera coordinate frame (do not confuse with inertial/world frame)
-     * @param y The Y coordinate of the point in the Camera coordinate frame (do not confuse with the inertial/world frame)
-     * @param z The Z coordinate of the point in the Camera coordinate frame (do not confuse with the inertial/world frame)
-     * @return Eigen::Vector2d The corresponding 2D pixel in the image frame
-     */
-    virtual Eigen::Vector2d project(const double & x, const double & y, const double & z) = 0;
-
-    /**
-     * @brief Method that takes a 3D point expressed in the Camera coordinate frame,
-     * projects it to the image plane (using the intrinsics), and outputs the pixels
-     * corresponding to that point in the image plane
+     * corresponding to that point in the image plane. (pi function)
      * @param point The 3D point expressed in the Camera coordinate frame (do not confuse with inertial/world frame)
-     * @return Eigen::Vector2d The corresponding 2D pixel in the image frame
+     * @param pixel Eigen::Vector2d The corresponding 2D pixel in the image frame
      */
-    virtual Eigen::Vector2d project(const Eigen::Vector3d & point) = 0;
+    virtual void project(const Eigen::Vector3d & point, Eigen::Vector2d & pixel) const = 0;
 
     /**
      * @brief Method that takes a 2D pixel in the image frame,
      * and backprojects it to the Camera coordinate frame. Since this problem 
-     * is under-defined, we only generate the normalized vector from the camera
-     * center to the point (expressed in the Camera frame, and which we don't know exactly
-     * where it is, but know its direction from the camera center)
-     * @param u The horizontal pixel
-     * @param v The vertical pixel
-     * @return Eigen::Vector3d The normalized 3D vector from the center of the camera
-     * coordinate frame to the 3D point (that we don't know the Z). This vector is returned
-     * normalized (as a unit vector)
-     */
-    virtual Eigen::Vector3d backproject(const double & u, const double & v) = 0;
-
-    /**
-     * @brief Method that takes a 2D pixel in the image frame,
-     * and backprojects it to the Camera coordinate frame. Since this problem 
-     * is under-defined, we only generate the normalized vector from the camera
+     * is under-defined, we only generate a vector from the camera
      * center to the point (expressed in the Camera frame, and which we don't know exactly
      * where it is, but know its direction from the camera center)
      * @param pixel A 2D vector which encodes the pixel (u=horizontal pixel, v=vertical pixel)
-     * @return Eigen::Vector3d The normalized 3D vector from the center of the camera
+     * @param vector The 3D vector from the center of the camera
+     * coordinate frame to the 3D point (that we don't know the Z).
+     */
+    virtual void backproject(const Eigen::Vector2d & pixel, Eigen::Vector3d & vector) const = 0;
+
+    /**
+     * @brief Method that takes a 2D pixel in the image frame,
+     * and backprojects it to the Camera coordinate frame. Since this problem
+     * is under-defined, we only generate the normalized vector from the camera
+     * center to the point (the same as backproject, but guarantees that this vector has norm=1)
+     * @param pixel A 2D vector which encodes the pixel (u=horizontal pixel, v=vertical pixel)
+     * @param Eigen::Vector3d The normalized 3D vector from the center of the camera
      * coordinate frame to the 3D point (that we don't know the Z). This vector is returned
      * normalized (as a unit vector)
-     */
-    virtual Eigen::Vector3d backproject(const Eigen::Vector2d & pixel) = 0;
+    */
+    virtual void backproject_unitsphere(const Eigen::Vector2d & pixel, Eigen::Vector3d & vector) const {
+        backproject(pixel, vector);
+        vector.normalize();
+    }
 
     /**
      * @brief Returns the width (in pixels) of the images produced by this camera
      */
-    inline unsigned int width() { return width_; }
+    inline unsigned int width() const { return width_; }
 
     /**
      * @brief Returns the height (in pixels) of the images produced by this camera
      */
-    inline unsigned int height() { return height_; }
+    inline unsigned int height() const { return height_; }
 
     /**
      * @brief Method that given a 2D vector representing a pixel coordinate, checks
@@ -83,7 +72,7 @@ public:
      * @param pixel The 2D coordinates of the pixel
      * @return bool A boolean whether the pixel lies in the image plane of this camera or not
      */
-    inline bool is_pixel_in_frame(const Eigen::Vector2d & pixel) { return is_pixel_in_frame(pixel[0], pixel[1]); }
+    inline bool is_pixel_in_frame(const Eigen::Vector2d & pixel) const { return is_pixel_in_frame(pixel[0], pixel[1]); }
 
     /**
      * @brief Method that given a 2D vector representing a pixel coordinate, checks
@@ -92,10 +81,9 @@ public:
      * @param v The "y" coordinate of the pixel in the image frame
      * @return bool A boolean whether the pixel lies in the image plane of this camera or not
      */
-    inline bool is_pixel_in_frame(const double & u, const double & v) {
+    inline bool is_pixel_in_frame(const double & u, const double & v) const {
         return (u >= 0.0 && u < width_ && v >= 0.0 && v < height_) ? true : false;
     }
-
 
 protected:
 
@@ -104,7 +92,7 @@ protected:
      * @param width The width of the camera image frame in pixels
      * @param height The height of the camera image frame in pixels
      */
-    Camera(const int &  width, const int & height) : width_(width), height_(height) {}
+    Camera(const unsigned int &  width, const unsigned int & height) : width_(width), height_(height) {}
 
     /**
      * @brief The dimensions of the image frame that this camera generates (in pixels)
--- pegasus_vision/camera_models/include/camera_models/pinhole_camera.hpp
+++ pegasus_vision/camera_models/include/camera_models/pinhole_camera.hpp
@@ -32,50 +32,23 @@ public:
     /**
      * @brief Method that takes a 3D point expressed in the Camera coordinate frame,
      * projects it to the image plane (using the intrinsics), and outputs the pixels
-     * corresponding to that point in the image plane
-     * @param x The X coordinate of the point in the Camera coordinate frame (do not confuse with inertial/world frame)
-     * @param y The Y coordinate of the point in the Camera coordinate frame (do not confuse with the inertial/world frame)
-     * @param z The Z coordinate of the point in the Camera coordinate frame (do not confuse with the inertial/world frame)
-     * @return Eigen::Vector2d The corresponding 2D pixel in the image frame
-     */
-    virtual Eigen::Vector2d project(const double & x, const double & y, const double & z) override;
-
-    /**
-     * @brief Method that takes a 3D point expressed in the Camera coordinate frame,
-     * projects it to the image plane (using the intrinsics), and outputs the pixels
-     * corresponding to that point in the image plane
+     * corresponding to that point in the image plane. (pi function)
      * @param point The 3D point expressed in the Camera coordinate frame (do not confuse with inertial/world frame)
-     * @return Eigen::Vector2d The corresponding 2D pixel in the image frame
+     * @param pixel Eigen::Vector2d The corresponding 2D pixel in the image frame
      */
-    virtual Eigen::Vector2d project(const Eigen::Vector3d & point) override;
+    virtual void project(const Eigen::Vector3d & point, Eigen::Vector2d & pixel) const override;
 
     /**
      * @brief Method that takes a 2D pixel in the image frame,
      * and backprojects it to the Camera coordinate frame. Since this problem 
-     * is under-defined, we only generate the normalized vector from the camera
+     * is under-defined, we only generate a vector from the camera
      * center to the point (expressed in the Camera frame, and which we don't know exactly
      * where it is, but know its direction from the camera center)
      * @param pixel A 2D vector which encodes the pixel (u=horizontal pixel, v=vertical pixel)
-     * @return Eigen::Vector3d The normalized 3D vector from the center of the camera
-     * coordinate frame to the 3D point (that we don't know the Z). This vector is returned
-     * normalized (as a unit vector)
+     * @param vector The 3D vector from the center of the camera
+     * coordinate frame to the 3D point (that we don't know the Z).
      */
-    virtual Eigen::Vector3d backproject(const Eigen::Vector2d & pixels) override;
-
-    /**
-     * @brief Method that takes a 2D pixel in the image frame,
-     * and backprojects it to the Camera coordinate frame. Since this problem 
-     * is under-defined, we only generate the normalized vector from the camera
-     * center to the point (expressed in the Camera frame, and which we don't know exactly
-     * where it is, but know its direction from the camera center)
-     * @param u The horizontal pixel
-     * @param v The vertical pixel
-     * @return Eigen::Vector3d The normalized 3D vector from the center of the camera
-     * coordinate frame to the 3D point (that we don't know the Z). This vector is returned
-     * normalized (as a unit vector)
-     */
-    virtual Eigen::Vector3d backproject(const double & u, const double & v) override;
-
+    virtual void backproject(const Eigen::Vector2d & pixel, Eigen::Vector3d & vector) const override;
 
 private:
 
