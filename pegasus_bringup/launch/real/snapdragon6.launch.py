0defef8ce03f681f400675006232dd8db10c3677
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2024-02-19 20:03:46 +0000

    feat: preparation pre-field trials

--- pegasus_bringup/launch/real/snapdragon6.launch.py
+++ pegasus_bringup/launch/real/snapdragon6.launch.py
@@ -5,7 +5,6 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.conditions import LaunchConfigurationEquals
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
@@ -16,7 +15,7 @@ def generate_launch_description():
     # --------------------------------
     
     # Set the default vehicle id (note: this is a trick due to the parameter reading limitation in ROS2)
-    default_vehicle_id = 6
+    default_vehicle_id = 1
     vehicle_id = default_vehicle_id
     for arg in sys.argv:
         if arg.startswith('vehicle_id:='):
@@ -25,26 +24,43 @@ def generate_launch_description():
     # ----------------------------------------
     # ---- DECLARE THE LAUNCH ARGUMENTS ------
     # ----------------------------------------
+    
+    # Define the standard mavlink port to forward mavlink data (so that it can also be viewed internally by qgroundcontrol)
+    udp_local_forward_port = 14559 + vehicle_id
+    udp_local_forward_adress = "udp://127.0.0.1:" + str(udp_local_forward_port)
+    mavlink_forward_addresses = "[" + udp_local_forward_adress + "]"
 
     # Namespace and ID of the vehicle as parameter received by the launch file
     id_arg = DeclareLaunchArgument('vehicle_id', default_value=str(vehicle_id), description='Drone ID in the network')
     namespace_arg = DeclareLaunchArgument('vehicle_ns', default_value='drone', description='Namespace to append to every topic and node name')
     
     # Define the drone MAVLINK IP and PORT
-    mav_connection_arg = DeclareLaunchArgument('connection', default_value='udp://:' + str(15000 + vehicle_id), description='The interface used to connect to the vehicle')
+    mav_connection_arg = DeclareLaunchArgument('connection', default_value='udp://:15006', description='The interface used to connect to the vehicle')
 
     # Define the drone MAVLINK forward ips and ports
-    mavlink_forward_arg = DeclareLaunchArgument('mavlink_forward', default_value="['udp://192.168.1.112:14550']", description='A list of ips where to forward mavlink messages')
+    mavlink_forward_arg = DeclareLaunchArgument('mavlink_forward', default_value=mavlink_forward_addresses, description='A list of ips where to forward mavlink messages')
     
     # Define which file to use for the drone parameters
     drone_params_file_arg = DeclareLaunchArgument(
         'drone_params', 
-        default_value=os.path.join(get_package_share_directory('pegasus_bringup'), 'config', 'snapdragon.yaml'),
+        default_value=os.path.join(get_package_share_directory('pegasus_bringup'), 'config', 'snapdragon6.yaml'),
         description='The directory where the drone parameters such as mass, thrust curve, etc. are defined')
     
     # ----------------------------------------
     # ---- DECLARE THE NODES TO LAUNCH -------
     # ----------------------------------------
+
+    # Call the MOCAP driver launch file
+    mocap_launch_file = IncludeLaunchDescription(
+        # Grab the launch file for the mocap interface
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('mocap_interface'), 'launch/vrpn.launch.py')),
+        # Define costume launch arguments/parameters used for the mocap interface
+        launch_arguments={
+            'id': LaunchConfiguration('vehicle_id'), 
+            'namespace': LaunchConfiguration('vehicle_ns')
+        }.items(),
+        #condition=LaunchConfigurationEquals('activate_mocap', 'True')
+    )
     
     # Call MAVLINK interface package launch file 
     mavlink_interface_launch_file = IncludeLaunchDescription(
@@ -59,27 +75,15 @@ def generate_launch_description():
             'mavlink_forward': LaunchConfiguration('mavlink_forward')
         }.items(),
     )
-    
-    # Call the MOCAP driver launch file
-    mocap_launch_file = IncludeLaunchDescription(
-        # Grab the launch file for the mocap interface
-        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('vrpn_client_ros'), 'launch/vrpn.launch.py')),
-        # Define costume launch arguments/parameters used for the mocap interface
-        launch_arguments={
-            'id': LaunchConfiguration('vehicle_id'), 
-            'namespace': LaunchConfiguration('vehicle_ns')
-        }.items(),
-        #condition=LaunchConfigurationEquals('activate_mocap', 'True')
-    )
 
-    # Call Path following/Trajectory tracking controllers package launch file
-    paths_launch_file = IncludeLaunchDescription(
-        # Grab the launch file for the paths following/trajectory tracking 
-        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('pegasus_paths'), 'launch/pegasus_paths.launch.py')),
+    # Call autopilot package launch file
+    autopilot_launch_file = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('autopilot'), 'launch/autopilot.launch.py')),
         # Define costume launch arguments/parameters used 
         launch_arguments={
             'id': LaunchConfiguration('vehicle_id'),
-            'namespace': LaunchConfiguration('vehicle_ns')
+            'namespace': LaunchConfiguration('vehicle_ns'),
+            'autopilot_yaml': LaunchConfiguration('drone_params'),
         }.items(),
     )
 
@@ -95,6 +99,6 @@ def generate_launch_description():
         drone_params_file_arg,
         # Launch files
         mavlink_interface_launch_file,
-        mocap_launch_file,
-        paths_launch_file
+        autopilot_launch_file,
+        mocap_launch_file
     ])
