c47c9d0adb2a479a50edbbcbb5dd2b7f8f7687cf
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2024-06-28 21:54:23 +0100

    feat: update for consensus

---
+++ pegasus_autopilot/autopilot_modes/src/mode_consensus.cpp
@@ -0,0 +1,90 @@
+/*****************************************************************************
+ * 
+ *   Author: Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
+ *   Copyright (c) 2024, Marcelo Jacinto. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright 
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ * notice, this list of conditions and the following disclaimer in 
+ * the documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this 
+ * software must display the following acknowledgement: This product 
+ * includes software developed by Project Pegasus.
+ * 4. Neither the name of the copyright holder nor the names of its 
+ * contributors may be used to endorse or promote products derived 
+ * from this software without specific prior written permission.
+ *
+ * Additional Restrictions:
+ * 4. The Software shall be used for non-commercial purposes only. 
+ * This includes, but is not limited to, academic research, personal 
+ * projects, and non-profit organizations. Any commercial use of the 
+ * Software is strictly prohibited without prior written permission 
+ * from the copyright holders.
+ * 5. The Software shall not be used, directly or indirectly, for 
+ * military purposes, including but not limited to the development 
+ * of weapons, military simulations, or any other military applications. 
+ * Any military use of the Software is strictly prohibited without 
+ * prior written permission from the copyright holders.
+ * 6. The Software may be utilized for academic research purposes, 
+ * with the condition that proper acknowledgment is given in all 
+ * corresponding publications.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************/
+#include "autopilot_modes/mode_consensus.hpp"
+
+namespace autopilot {
+
+ConsensusMode::~ConsensusMode() {}
+
+void ConsensusMode::initialize() {
+
+    // Initialize the ROS 2 service clients
+    //node_->declare_parameter<std::string>("autopilot.ArmMode.arm_service", "arm");
+    //node_->declare_parameter<std::string>("autopilot.ArmMode.offboard_service", "offboard");
+
+    // ----- THIS CODE IS ONLY USED IN ROS FOXY --------
+    std::string sub_node_name = "autopilot_consensus_client";
+    auto options = rclcpp::NodeOptions()
+        .start_parameter_services(false)
+        .start_parameter_event_publisher(false)
+        .arguments({"--ros-args", "-r", "__node:=" + sub_node_name, "--"});
+
+    sub_node_ = rclcpp::Node::make_shared("_", options);
+ 
+    // Log that the ArmMode has been initialized successfully 
+    RCLCPP_INFO(this->node_->get_logger(), "ConsensusMode initialized");
+}
+
+bool ConsensusMode::enter() {
+
+    return true;
+}
+
+bool ConsensusMode::exit() {
+    // Do nothing, just return true
+    return true;
+}
+
+void ConsensusMode::update(double) {
+   return;
+}
+
+} // namespace autopilot
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(autopilot::ConsensusMode, autopilot::Mode)
